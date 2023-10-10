1d2fddd11e9d7eb2faacdbf64d2ebe35e31510a5
Rajib Pokhrel <pokhrelrajib015@gmail.com>
2023-10-10 15:56:39 +0545

    day2

---
+++ src/components/github/Github.jsx
@@ -0,0 +1,5 @@
+export default function Github() {
+    return(
+        <h1>HI i am git hub</h1>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import "./index.css"
+import { createRoot } from 'react-dom/client';
+import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
+import Contact from './components/contact/Contact.jsx';
+import Github from './components/github/Github.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path:"",
+        element:<Home />
+      },
+      {
+        path:"about",
+        element:<About />
+      },
+      {
+        path:"contact",
+        element:<Contact />
+      },
+      {
+        path:"github",
+        element:<Github />
+      }
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
